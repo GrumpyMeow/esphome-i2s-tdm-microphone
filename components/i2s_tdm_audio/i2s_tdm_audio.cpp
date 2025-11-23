6c168cd5653874cd20753913671262b9dca1b92b
Meow <sander.schutten@gmail.com>
2025-11-23 13:15:02 +0100

    Update i2s_tdm_audio.cpp

--- components/i2s_tdm_audio/i2s_tdm_audio.cpp
+++ components/i2s_tdm_audio/i2s_tdm_audio.cpp
@@ -12,7 +12,7 @@ static const char *const TAG = "i2s_tdm_audio";
 void I2STDMAudioComponent::setup() {
   static i2s_port_t next_port_num = I2S_NUM_0;
   if (next_port_num >= SOC_I2S_NUM) {
-    ESP_LOGE(TAG, "Too many components");
+    ESP_LOGE(TAG, "No unused i2s-port available");
     this->mark_failed();
     return;
   }
