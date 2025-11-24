e48bbe28442d44d5bc5962441802c54ac3ce0d4e
Meow <sander.schutten@gmail.com>
2025-11-24 15:42:10 +0100

    Update __init__.py

--- components/i2s_tdm_audio/__init__.py
+++ components/i2s_tdm_audio/__init__.py
@@ -254,5 +254,5 @@ async def to_code(config):
     i2s_tdm_slot_mask_t = cg.global_ns.enum("i2s_tdm_slot_mask_t")
     slot_mask_value = i2s_tdm_slot_mask_t()
     for slot in config[CONF_SLOTS]:
-       slot_mask_value = slot_mask_value | I2S_TDM_SLOT_MASK[slot]
+       slot_mask_value = i2s_tdm_slot_mask_t(slot_mask_value.value) | I2S_TDM_SLOT_MASK[slot]
     cg.add(var.set_tdm_slot_mask(slot_mask_value))
